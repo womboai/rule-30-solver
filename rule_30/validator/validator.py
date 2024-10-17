fixed score normalization

rule_30/validator/validator.py
@@ -238,26 +238,43 @@ class Validator:
             except:
                 logger.error(f"Error in evolution step {self.step}", exc_info=True)
     
-    async def normalize_scores(self, outputs: np.ndarray) -> np.ndarray:
+
+    async def normalize_scores(self, outputs: np.ndarray[np.ndarray[np.uint64]]) -> np.ndarray[np.uint64]:
         def rule_30(a: np.uint64) -> np.uint64:
-            return a^(a<<1 | a<<2)
+            return np.uint64(a ^ (a << np.uint64(1) | a << np.uint64(2)))
         def normalize_pair(a: np.uint64, b: np.uint64) -> tuple[np.uint64, np.uint64]:
-            carry = a & 1
-            a = a >> 1
-            b = (carry << (b.bit_length())) | b
-            a, b = (rule_30(a), rule_30(b))
-            msb = b >> (b.dtype.itemsize * 8 - 1)
-            b = b & ((1 << (b.dtype.itemsize * 8 - 1)) - 1)
-            a = (a << 1) | msb
+            carry = np.uint64(a & np.uint64(1))
+
+            a = np.uint64(a >> np.uint64(1))
+            b = np.uint64((carry << np.uint64(63)) | b)
+
+            a = rule_30(a)
+            b = rule_30(b)
+
+            msb = np.uint64(b >> np.uint64(63))
+
+            b = np.uint64(b & np.uint64((1 << 63) - 1))
+
+            a = np.uint64((a << np.uint64(1)) | msb)
+
             return a, b
+
         normalized_outputs = []
-        for i in range(len(outputs)-2):
-            a,b = normalize_pair(outputs[i][-1],outputs[i+1][0])
+
+        for i in range(len(outputs) - 1): 
+            a = np.uint64(outputs[i][-1])
+            b = np.uint64(outputs[i + 1][0])
+
+            a, b = normalize_pair(a, b)
+
             outputs[i][-1] = a
-            outputs[i+1][0] = b
-            normalized_outputs+=outputs[i]
-        normalized_outputs.append(outputs[-1])
-        return np.array(normalized_outputs, dtype=np.uint64)[0]
+            outputs[i + 1][0] = b
+
+        normalized_outputs.extend(outputs[i].astype(np.uint64))
+
+        normalized_outputs.extend(outputs[-1].astype(np.uint64))
+
+        return np.array(normalized_outputs, dtype=np.uint64)
 
 def main():
     asyncio.run(Validator().run())