cc6f8185a1937cb4d28870c058dbadc13808c1fe
alex n <alexander.neagoe@gmail.com>
2024-10-16 19:32:58 -0700

    fixed normalize scores

--- rule_30/validator/validator.py
+++ rule_30/validator/validator.py
@@ -239,21 +239,25 @@ class Validator:
                 logger.error(f"Error in evolution step {self.step}", exc_info=True)
     
     async def normalize_scores(self, outputs: np.ndarray) -> np.ndarray:
+        def rule_30(a: np.uint64) -> np.uint64:
+            return a^(a<<1 | a<<2)
         def normalize_pair(a: np.uint64, b: np.uint64) -> tuple[np.uint64, np.uint64]:
             carry = a & 1
             a = a >> 1
             b = (carry << (b.bit_length())) | b
-            a, b = sample_miner(np.array([a, b], dtype=np.uint64))
+            a, b = (rule_30(a), rule_30(b))
             msb = b >> (b.dtype.itemsize * 8 - 1)
             b = b & ((1 << (b.dtype.itemsize * 8 - 1)) - 1)
             a = (a << 1) | msb
             return a, b
         normalized_outputs = []
         for i in range(len(outputs)-2):
-            a,b = normalize_pair(outputs[i][0],outputs[i+1][-1])
-            normalized_outputs.append(a)
-            normalized_outputs.append(b)
-        return normalized_outputs
+            a,b = normalize_pair(outputs[i][-1],outputs[i+1][0])
+            outputs[i][-1] = a
+            outputs[i+1][0] = b
+            normalized_outputs+=outputs[i]
+        normalized_outputs.append(outputs[-1])
+        return np.array(normalized_outputs, dtype=np.uint64)[0]
 
 def main():
     asyncio.run(Validator().run())
