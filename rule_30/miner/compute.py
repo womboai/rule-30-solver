9a17524db547e7479044b054a3008ac29f46f1ae
alex. <131470654+3xela@users.noreply.github.com>
2024-10-17 19:55:33 -0700

    fix miner bug (#2)
    i fixed the miner bug, just changed a function so it should work now.

--- rule_30/miner/compute.py
+++ rule_30/miner/compute.py
@@ -8,6 +8,9 @@ def compute_response(data: NDArray[np.uint64]) -> NDArray[np.uint64]:
     raw_bytes = data.tobytes()
     combined_int = int.from_bytes(raw_bytes, "little")
     transformed_int = combined_int ^ ((combined_int << 1) | (combined_int << 2))
-    transformed_bytes = transformed_int.to_bytes(len(raw_bytes), 'little')
-
+    
+    num_bytes = (transformed_int.bit_length() + 7) // 8
+    num_bytes += (8 - num_bytes % 8)   
+    
+    transformed_bytes = transformed_int.to_bytes(num_bytes, 'little')
     return np.frombuffer(transformed_bytes, dtype=np.uint64)
